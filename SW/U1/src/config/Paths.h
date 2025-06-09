3ab8ae39b10f1cfc4ec4382764a5d6ef3fa7593d
Yuanze-Li <yuanze.li@alumnos.upm.es>
2025-06-09 13:13:11 +0200

    Pa

--- SW/U1/src/config/Paths.h
+++ SW/U1/src/config/Paths.h
@@ -20,8 +20,10 @@
 	#define PATH_MEMORIA    "C:/Users/Fabian/Desktop/workspace/UPM/Laboratorio/ISE/Bloque_II/ise_grupo_l7/SW/U1/src/memoria/Memoria.h"
 	#define PATH_COM_PLACAS "C:/Users/Fabian/Desktop/workspace/UPM/Laboratorio/ISE/Bloque_II/ise_grupo_l7/SW/U1/src/com_placas/ComunicacionPlacas.h"
 	#define PATH_PRINCIPAL  "C:/Users/Fabian/Desktop/workspace/UPM/Laboratorio/ISE/Bloque_II/ise_grupo_l7/SW/U1/src/juego/principal.h"
+	#define PATH_COMMON		"C:/Users/Fabian/Desktop/workspace/UPM/Laboratorio/ISE/Bloque_II/ise_grupo_l7/SW/U1/src/common/CommonDefines.h"
 
 #elif WORKSPACE == 2		// Yuanze
+<<<<<<< HEAD
 	#define PATH_MAIN        "D:/Estudio/ISE/Bloq2/ise_grupo_l7/SW/U1/src/main.h"
 	#define PATH_SERVER      "D:/Estudio/ISE/Bloq2/ise_grupo_l7/SW/U1/src/servidor/server.h"
 	#define PATH_ADC         "D:/Estudio/ISE/Bloq2/ise_grupo_l7/SW/U1/src/adc/adc.h"
@@ -37,6 +39,24 @@
 	#define PATH_MEMORIA     "D:/Estudio/ISE/Bloq2/ise_grupo_l7/SW/U1/src/memoria/Memoria.h"
 	#define PATH_COM_PLACAS  "D:/Estudio/ISE/Bloq2/ise_grupo_l7/SW/U1/src/com_placas/ComunicacionPlacas.h"
 	#define PATH_PRINCIPAL   "D:/Estudio/ISE/Bloq2/ise_grupo_l7/SW/U1/src/juego/principal.h"
+=======
+	#define PATH_MAIN        "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/main.h"
+	#define PATH_SERVER      "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/servidor/server.h"
+	#define PATH_ADC         "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/adc/adc.h"
+	#define PATH_BUTTONS     "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/buttons/buttons.h"
+	#define PATH_NET_CONFIG  "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/config/NetConfig.h"
+	#define PATH_IRQ         "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/irq/stm32f4xx_it.h"
+	#define PATH_LCD         "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/lcd/lcd.h"
+	#define PATH_LED         "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/led/led.h"
+	#define PATH_LED_STRIP   "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/led/LedStripManager.h"
+	#define PATH_RTC         "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/rtc/rtc.h"
+	#define PATH_UART        "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/uart/uart.h"
+	#define PATH_POSITION    "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/posicion/PositionManager.h"
+	#define PATH_MEMORIA     "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/memoria/Memoria.h"
+	#define PATH_COM_PLACAS  "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/com_placas/ComunicacionPlacas.h"
+	#define PATH_PRINCIPAL   "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/juego/principal.h"
+	#define PATH_COMMON		 "D:/Estudio/ISE/Bloq2/dev/ise_grupo_l7/SW/U1/src/common/CommonDefines.h"
+>>>>>>> e3164773e5acb414be251954bf69d0f5bfefd78f
 
 #elif WORKSPACE == 3		// Raul
 	#define PATH_MAIN        "C:/Users/Olivia/Desktop/UNIVERSIDAD/10o_SEMESTRE/ISE/MASTERCHESS/ise_grupo_l7/SW/U1/src/main.h"
@@ -52,6 +72,7 @@
 	#define PATH_UART        "C:/Users/Olivia/Desktop/UNIVERSIDAD/10o_SEMESTRE/ISE/MASTERCHESS/ise_grupo_l7/SW/U1/src/uart/uart.h"
 	#define PATH_POSITION    "C:/Users/Olivia/Desktop/UNIVERSIDAD/10o_SEMESTRE/ISE/MASTERCHESS/ise_grupo_l7/SW/U1/src/posicion/PositionManager.h"
 	#define PATH_PRINCIPAL   "C:/Users/Olivia/Desktop/UNIVERSIDAD/10o_SEMESTRE/ISE/MASTERCHESS/ise_grupo_l7/SW/U1/src/juego/principal.h"
+	#define PATH_COMMON		 "C:/Users/Olivia/Desktop/UNIVERSIDAD/10o_SEMESTRE/ISE/MASTERCHESS/ise_grupo_l7/SW/U1/src/common/CommonDefines.h"
 
 #endif
 
